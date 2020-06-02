c99a94fc71c0a1e924ac884f69ecb6c58951fc56
Anh Vuong <vuonga1103@yahoo.com>
2020-06-02 15:25:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,15 +11,17 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop
+  array
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
